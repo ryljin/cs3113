b5dcc5471f8b1c504556c08385371393f6aefb4e
ryljin <ryanjin102354@gmail.com>
2026-02-28 20:41:12 -0800

    Update main.cpp

--- homework3/main.cpp
+++ homework3/main.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Ryan Jin
+* Assignment: Pong Clone
+* Date due: 02/28/2026
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #include "CS3113/Entity.h"
 #include <vector>
 #include <ctime>
